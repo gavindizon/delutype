e0497583e5573c99f516b4c12c134a7dab646fd4
Gavin Raine Dizon <60138634+gavindizon@users.noreply.github.com>
2022-08-06 02:15:33 +0800

    Added great shit

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -3,12 +3,6 @@ import { axiosInstance as axios } from "../../config/axios";
 import { useDispatch, useSelector } from "react-redux";
 
 import Layout from "../../components/Layout/Layout";
-import Button from "../../components/Button/Button";
-
-import { FaChevronDown } from "react-icons/fa";
-import { FcGoogle } from "react-icons/fc";
-import { CgMail } from "react-icons/cg";
-import { IoPersonCircleSharp } from "react-icons/io5";
 
 import Script from "next/script";
 
@@ -17,23 +11,30 @@ import useLanguage from "../../hooks/useLanguage";
 import TypingGame from "../../components/TypingGame";
 //import webgazer from "../../webgazer-v2/src/index.mjs";
 import React, { useEffect } from "react";
-import timeout from "../../utils/timeout";
+import { useState } from "react";
 
 const Test: NextPage = ({ ssr }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+    const [gazeCount, setGazeCount] = useState(0);
     const lang = useLanguage();
     const state = useSelector((state: any) => state);
     const dispatch = useDispatch();
     const genLang = general[lang as keyof typeof general];
+    let data = [];
 
-
-    const handleOnClick = () => {};
+    useEffect(() => {
+        window.addEventListener("addGaze", function () {
+            setGazeCount((prev) => prev + 1);
+            //data.push(gazeCount);
+            //console.log(gazeCount);
+        });
+    }, []);
 
     return (
         <Layout title="Test" description="" lang={lang} state={state} dispatch={dispatch}>
-              <Script src="/test.js" strategy="beforeInteractive" />
+            {/* <Script src="/test.js" strategy="beforeInteractive" /> */}
             <section className="h-screen px-2 text-center flex flex-col -mt-6 justify-center items-center relative">
-                <h5>Esc to reset</h5>
-                <button onClick={(e) => handleOnClick()}>X</button>
+                <h3>Gaze Counts: {gazeCount}</h3>
+
                 <div className="w-full md:w-1/2">
                     <h1 className="mb-2">Charles Darwin, On the Origin of Species</h1>
                     <TypingGame
---
+++ redux/actions/test.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+exports.addGazeCount = exports.ADD_GAZE_COUNT = void 0;
+exports.ADD_GAZE_COUNT = "ADD_GAZE_COUNT";
+var addGazeCount = function () { return function (dispatch) { return dispatch({ type: exports.ADD_GAZE_COUNT }); }; };
+exports.addGazeCount = addGazeCount;
