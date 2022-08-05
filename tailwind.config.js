73449aff39e7ff187c1f5ecb69e6c307122488de
Gavin Raine Dizon <60138634+gavindizon@users.noreply.github.com>
2022-08-05 13:47:08 +0800

    for deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+    purge: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./src/components/**/*.{js,ts,jsx,tsx}",
+        "./src/**/*.{js,ts,jsx,tsx}",
+        "./src/**/*.{js,ts,jsx,tsx}",
+        "./src/**/*.{js,ts,jsx,tsx}",
+        "./public/**/*.{js,ts,jsx,tsx, html}",
+    ],
     theme: {
         extend: {},
     },
