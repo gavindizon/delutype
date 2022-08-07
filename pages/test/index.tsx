8471fdcb8d875ae0a3ae9c8d2c590e17b8997242
Gavin Raine Dizon <60138634+gavindizon@users.noreply.github.com>
2022-08-07 22:46:40 +0800

    Updated test route

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,85 +1,45 @@
-import type {
-  NextPage,
-  GetServerSideProps,
-  InferGetServerSidePropsType,
-} from "next";
+import type { NextPage, GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { axiosInstance as axios } from "../../config/axios";
 import { useDispatch, useSelector } from "react-redux";
 
 import Layout from "../../components/Layout/Layout";
-
-import Script from "next/script";
-
 import general from "../../data/general.json";
 import useLanguage from "../../hooks/useLanguage";
 import TypingGame from "../../components/TypingGame";
-//import webgazer from "../../webgazer-v2/src/index.mjs";
 import React, { useEffect } from "react";
 import { useState } from "react";
 
-const Test: NextPage = ({
-  ssr,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  const [gazeCount, setGazeCount] = useState(0);
-  const lang = useLanguage();
-  const state = useSelector((state: any) => state);
-  const dispatch = useDispatch();
-  const genLang = general[lang as keyof typeof general];
-  let data = [];
+const Test: NextPage = ({ text, title }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+    const lang = useLanguage();
+    const state = useSelector((state: any) => state);
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-    window.addEventListener("addGaze", function () {
-      setGazeCount((prev) => prev + 1);
-      //data.push(gazeCount);
-      //console.log(gazeCount);
-    });
-  }, []);
-
-  return (
-    <Layout
-      title="Test"
-      description=""
-      lang={lang}
-      state={state}
-      dispatch={dispatch}
-    >
-      {/* <Script src="/test.js" strategy="beforeInteractive" /> */}
-      <section className="h-screen px-2 text-center flex flex-col -mt-6 justify-center items-center relative">
-        <h3>Gaze Counts: {gazeCount}</h3>
-
-        <div className="w-full md:w-1/2">
-          <h1 className="mb-2">Charles Darwin, On the Origin of Species</h1>
-          <TypingGame
-            text={
-              "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat eum, qui nulla culpa dignissimos soluta excepturi omnis possimus repudiandae, incidunt molestias? Eius aperiam accusamus unde laboriosam totam. Voluptatum, accusamus eius!"
-            }
-          />
-          
-          {/* <TypingGame
-            text={
-              "Lorem ipsum dolor sit amet"
-            }
-          /> */}
-        </div>
-      </section>
-    </Layout>
-  );
+    return (
+        <Layout title="Test" description="" lang={lang} state={state} dispatch={dispatch}>
+            <section className="h-screen px-2 text-center flex flex-col -mt-6 justify-center items-center relative">
+                <div className="w-full md:w-1/2">
+                    <h1 className="mb-2">{title}</h1>
+                    <TypingGame text={text} />
+                </div>
+            </section>
+        </Layout>
+    );
 };
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  try {
-    const data = 1;
-    //const { data } =  //await axios.get("https://pokeapi.co/api/v2/pokemon/ditto");
-    return {
-      props: {
-        ssr: data,
-      },
-    };
-  } catch (e) {
-    return {
-      props: {},
-    };
-  }
+    try {
+        const data = 1;
+        return {
+            props: {
+                title: "Lorem ipsum title",
+                text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+            },
+        };
+    } catch (e) {
+        return {
+            props: {},
+        };
+    }
 };
 
 export default Test;
--- public/test.js
+++ public/test.js
@@ -1,25 +1,10 @@
 var addGaze = new CustomEvent("addGaze");
-console.log(window);
-// window.addEventListener(
-//     "addGaze",
-//     () => {
-//         console.log("Test");
-//     },
-//     false
-// );
 
+const BOTTOM = window.innerHeight * 0.8;
 (function gazer() {
-    webgazer
-        .removeMouseEventListeners()
-        .setGazeListener(function (data, elapsedTime) {
-            if (data === null) return;
-            var xprediction = data.x; //these x coordinates are relative to the viewport
-            var yprediction = data.y; //these y coordinates are relative to the viewport
-            //console.log(elapsedTime); //elapsed time is based on time since begin was called
-            //console.log(yprediction);
-            if (yprediction >= 600) {
-                dispatchEvent(addGaze);
-            }
-        })
-        .begin();
+    webgazer.setGazeListener(function (data, elapsedTime) {
+        //        if (data === null) return;
+        var yprediction = data?.y;
+        if (yprediction >= BOTTOM) dispatchEvent(addGaze);
+    });
 })();
