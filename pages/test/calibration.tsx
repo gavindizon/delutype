e4682532097eae5c73b148f327e5721207136c81
Gavin Raine Dizon <60138634+gavindizon@users.noreply.github.com>
2022-08-21 01:24:59 +0800

    fixed layout old props

--- pages/test/calibration.tsx
+++ pages/test/calibration.tsx
@@ -15,81 +15,66 @@ import { useState } from "react";
 import CalibrationButton from "../../components/CalibrationButton/calibrationButton";
 import Modal from "../../components/Modal/Modal";
 const Calibration: NextPage = ({ ssr }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-
     useEffect(() => {
         webgazer.begin();
-    
+
         return () => {
-          webgazer.end();
+            webgazer.end();
         };
-      }, []);
+    }, []);
 
     const [gazeCount, setGazeCount] = useState(0);
     const lang = useLanguage();
-    const state = useSelector((state: any) => state);
-    const dispatch = useDispatch();
     const genLang = general[lang as keyof typeof general];
     let data = [];
 
-    const [remaining, setRemaining] = useState(9)
+    const [remaining, setRemaining] = useState(9);
 
     // useEffect(() => {
-      
+
     //         dispatch({ type: "OPEN_MODAL" });
-        
+
     //   }, []);
 
     // useEffect(() => {
-      
+
     //     if (remaining === 0){
 
     //         dispatch({ type: "OPEN_MODAL" });
 
     //     }
-    
-    //   }, [remaining]);
-    
-    return (
-        
-        
 
-        <Layout title="Calibration" description="" lang={lang} state={state} dispatch={dispatch}>
-           
+    //   }, [remaining]);
+
+    return (
+        <Layout title="Calibration" description="" lang={lang}>
             <section>
                 <h3 className="text-center text-3xl pt-10">Calibration</h3>
-            
-            <div className="flex flex-row h-fit py-5 w-full justify-between">
-    
-            <CalibrationButton  id="btn1"  setRemaining={setRemaining}></CalibrationButton>
-                
-            <CalibrationButton   id="btn2" setRemaining={setRemaining}></CalibrationButton>
-            
-            <CalibrationButton   id="btn3" setRemaining={setRemaining}></CalibrationButton>
-           
-            </div>
 
-            <div className="flex flex-row h-fit pt-96 w-full justify-between">
+                <div className="flex flex-row h-fit py-5 w-full justify-between">
+                    <CalibrationButton id="btn1" setRemaining={setRemaining}></CalibrationButton>
 
-            <CalibrationButton  id="btn4" setRemaining={setRemaining}></CalibrationButton>
+                    <CalibrationButton id="btn2" setRemaining={setRemaining}></CalibrationButton>
 
-            <CalibrationButton  id="btn5" hide={remaining > 1} setRemaining={setRemaining}></CalibrationButton>
-       
-            <CalibrationButton  id="btn6" setRemaining={setRemaining}></CalibrationButton>
-           
-            </div>  
+                    <CalibrationButton id="btn3" setRemaining={setRemaining}></CalibrationButton>
+                </div>
 
-            <div className="flex flex-row h-fit fixed bottom-10 w-full justify-around pl-56 xl:pl-12 2xl:pr-24">
-       
-            <CalibrationButton   id="btn7" setRemaining={setRemaining}></CalibrationButton>
-          
-            <CalibrationButton   id="btn8" setRemaining={setRemaining}></CalibrationButton>
-           
-            <CalibrationButton   id="btn9" setRemaining={setRemaining}></CalibrationButton>
-          
-            </div> 
+                <div className="flex flex-row h-fit pt-96 w-full justify-between">
+                    <CalibrationButton id="btn4" setRemaining={setRemaining}></CalibrationButton>
 
+                    <CalibrationButton id="btn5" hide={remaining > 1} setRemaining={setRemaining}></CalibrationButton>
+
+                    <CalibrationButton id="btn6" setRemaining={setRemaining}></CalibrationButton>
+                </div>
+
+                <div className="flex flex-row h-fit fixed bottom-10 w-full justify-around pl-56 xl:pl-12 2xl:pr-24">
+                    <CalibrationButton id="btn7" setRemaining={setRemaining}></CalibrationButton>
+
+                    <CalibrationButton id="btn8" setRemaining={setRemaining}></CalibrationButton>
+
+                    <CalibrationButton id="btn9" setRemaining={setRemaining}></CalibrationButton>
+                </div>
             </section>
-
         </Layout>
     );
 };
