2e6c62a514fac23b86e33ff37bbf24083f101d8a
Gavin Raine Dizon <60138634+gavindizon@users.noreply.github.com>
2022-09-03 21:24:26 +0800

    add privacy policy page

--- public/test.js
+++ public/test.js
@@ -2,6 +2,7 @@ var addGaze = new CustomEvent("addGaze");
 
 const BOTTOM = window.innerHeight * 0.8;
 (function gazer() {
+    console.log(webgazer);
     webgazer.setGazeListener(function (data, elapsedTime) {
         //        if (data === null) return;
         var yprediction = data?.y;
